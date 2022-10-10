ef8a61d7ac0915a375ee50f4a4e9341e45369429
marllonus <kase@tuta.io>
2022-10-10 23:40:14 +0300

    add collection model

---
+++ app/Models/Collections.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Collections extends Model
+{
+    use HasFactory;
+
+    protected $guarded = ['id'];
+    protected $fillable = [
+        'url_code',
+        'name',
+        'description',
+    ];
+}
---
+++ database/migrations/2022_10_10_184540_create_collections_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('collections', function (Blueprint $table) {
+            $table->id();
+            $table->string('url_code');
+            $table->string('name');
+            $table->string('description')
+                  ->nullable();
+
+            $table->timestamp('created_at')
+                  ->useCurrent();
+            $table->timestamp('updated_at')
+                  ->useCurrent()
+                  ->useCurrentOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('collections');
+    }
+};
