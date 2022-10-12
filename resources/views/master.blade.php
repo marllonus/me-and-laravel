901387416a1cdf75f05e025a8722012a31bc9791
marllonus <kase@tuta.io>
2022-10-13 02:18:54 +0300

    font awesom compiled with vite

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -3,8 +3,9 @@
 <head>
     
     <title>Master page</title>
+    <!--<link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">-->
     @livewireStyles
-    @vite('resources/css/app.scss')
+    @vite(['resources/css/app.scss', 'node_modules/font-awesome/scss/font-awesome.scss'])
 </head>
 
 <body>
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,11 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.scss', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.scss', 
+                'resources/js/app.js',
+                'node_modules/font-awesome/scss/font-awesome.scss'
+            ],
             refresh: true,
         }),
     ],
