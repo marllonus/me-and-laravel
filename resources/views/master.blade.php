676e73e955341b6dee339d94fcd2d05d22ca2b08
marllonus <kase@tuta.io>
2022-10-12 04:31:17 +0300

    add vite styles to  master page

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -4,14 +4,14 @@
     
     <title>Master page</title>
     @livewireStyles
-
+    @vite('resources/css/app.scss')
 </head>
 
 <body>
     
     {{ $slot  }}
     @livewireScripts
-
+    @vite('resources/js/app.js')
 </body>
 
 </html>
