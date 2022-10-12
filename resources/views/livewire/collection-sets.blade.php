f9f8704a547cba6350904cd5c5f98bf0f2484389
marllonus <kase@tuta.io>
2022-10-13 02:50:15 +0300

    add buttonf  for collection sets  cards

--- resources/views/livewire/collection-sets.blade.php
+++ resources/views/livewire/collection-sets.blade.php
@@ -1,18 +1,27 @@
 <div class="container">
-  <header>
+  <!--<header>
     <h1>Responsive CSS Cards</h1>
     <p>by <a href="http://haneenkrimly.com" target="_blank">Haneen Krimly</a></p>
-  </header>
+  </header>-->
   
-  <div class="content">
+  <div class="content category-wrap">
     
-    <div class="card card-blue-light">
-      <h3>Card One</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-      <div class="pad-top"><a href="#" class="button">Learn More</a></div>
+    <div class="card card-blue-light category">
+      <h3>Title</h3>
+      <p>Text</p>
+      <div class="pad-top"><a href="#" class="button">Pick in</a></div>
+
+      <div class="links-wrap statistic-wrap">
+        <div class="toggle"><i class="fa"></i></div>
+        <div class="links">
+            <a href="#"><i class="fa fa-plus"></i></a>
+                <a href="#"><i class="fa fa-star"></i></a>
+                <a href="#"><i class="fa fa-mail-reply"></i></a>
+        </div>
+      </div>
     </div>
     
-    <div class="card card-yellow">
+    <!--<div class="card card-yellow">
       <h3>Card Two</h3>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
       <div class="pad-top"><a href="#" class="button">Learn More</a></div>
@@ -40,7 +49,7 @@
       <h3>Card Six</h3>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
       <div class="pad-top"><a href="#" class="button">Learn More</a></div>
-    </div>
+    </div>-->
     
   </div>
 </div>
