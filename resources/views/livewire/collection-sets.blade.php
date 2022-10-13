d2e4d454bbadac1d2527ba00b62f0960fb421bb2
marllonus <kase@tuta.io>
2022-10-13 03:04:53 +0300

    clean collection sets template

--- resources/views/livewire/collection-sets.blade.php
+++ resources/views/livewire/collection-sets.blade.php
@@ -14,42 +14,11 @@
       <div class="links-wrap statistic-wrap">
         <div class="toggle"><i class="fa"></i></div>
         <div class="links">
-            <a href="#"><i class="fa fa-plus"></i></a>
-                <a href="#"><i class="fa fa-star"></i></a>
-                <a href="#"><i class="fa fa-mail-reply"></i></a>
+            <a href="#"><i class="fa fa-pencil"></i></a>
+            <a href="#"><i class="fa fa-trash"></i></a>
         </div>
       </div>
     </div>
-    
-    <!--<div class="card card-yellow">
-      <h3>Card Two</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-      <div class="pad-top"><a href="#" class="button">Learn More</a></div>
-    </div>
-    
-    <div class="card card-blue-dark">
-      <h3>Card Three</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-      <div class="pad-top"><a href="#" class="button">Learn More</a></div>
-    </div>
-    
-    <div class="card card-red">
-      <h3>Card Four</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-      <div class="pad-top"><a href="#" class="button">Learn More</a></div>
-    </div>
-    
-    <div class="card card-purple">
-      <h3>Card Five</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-      <div class="pad-top"><a href="#" class="button">Learn More</a></div>
-    </div>
-    
-    <div class="card card-orange">
-      <h3>Card Six</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-      <div class="pad-top"><a href="#" class="button">Learn More</a></div>
-    </div>-->
     
   </div>
 </div>
