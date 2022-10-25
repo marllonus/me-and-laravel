482854017af0c58857c84c02c95c2ea4df66c0c4
marllonus <kase@tuta.io>
2022-10-25 03:39:19 +0300

    add some stuff template for rendering knowledge base set (draft)

--- resources/views/livewire/collections-page/collection-set.blade.php
+++ resources/views/livewire/collections-page/collection-set.blade.php
@@ -1,9 +1,21 @@
 <div class="knowledge-collection__set">
-    <h1 class="knowledge-collection__header">a collection set</h1>
-    <button wire:click="addCollection">add collection</button>
-    <div class="knowledge-collection__container">
-        @foreach($collections as $collection)
-            @livewire('collections-page.collection-card')
-        @endforeach
-    </div>
+    
+    <hr><br>
+    @if(isset($set->id))
+    
+        <h1 class="knowledge-collection__header">{{$set->title}}</h1>
+
+        <button wire:click="addCollection">add collection</button>
+
+        <div class="knowledge-collection__container">
+            @if(false)
+            @foreach($collections as $collection)
+                @livewire('collections-page.collection-card')
+            @endforeach
+            @endif
+        </div>
+    @else
+        <input type="text" wire:model="set.title">
+        <button>save</button>
+    @endif
 </div>
