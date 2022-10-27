aa522ea8a6226f2f4023bff450c21546ef03cefd
marllonus <kase@tuta.io>
2022-10-28 02:53:58 +0300

    add keys on loop for rendering all new added components without any errors

--- app/Http/Livewire/CollectionsPage/CollectionList.php
+++ app/Http/Livewire/CollectionsPage/CollectionList.php
@@ -9,6 +9,13 @@ class CollectionList extends Component
 {
     public $sets;
 
+    public function rules()
+    {
+        return  [
+            'sets.*.title' => 'nullable|string',
+        ];
+    }
+
     public function mount()
     {
         $this->sets = KnowledgeSet::all();
@@ -16,7 +23,12 @@ class CollectionList extends Component
 
     public function addSet()
     {
-       $this->sets->push(new KnowledgeSet);
+        $this->sets->push(new KnowledgeSet(['title' => 'new one']));
+    }
+
+    public function removeSet($key)
+    {
+        dd($key);
     }
 
     public function render()
--- resources/views/livewire/collections-page/collection-list.blade.php
+++ resources/views/livewire/collections-page/collection-list.blade.php
@@ -1,7 +1,8 @@
 <div class="knowledge-collection">
     <button wire:click="addSet">add set</button>
-    @foreach($sets as $set)
-        @livewire('collections-page.collection-set')
+    @foreach($sets as $key=>$set)
+        @livewire('collections-page.collection-set', 
+                            ['set' => $set], key($key))
     @endforeach
 
 </div>
